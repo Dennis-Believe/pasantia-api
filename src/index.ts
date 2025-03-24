257b9a77c410aa2d73229216e99af3c7bd786800
Dennis Laurel <dennis.laurel.v@gmail.com>
2025-03-24 19:49:49 -0400

    configurando ENV

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+export type Env = {
+  Bindings: {
+    DATABASE_URL: string
+  }
+}
+
+const app = new Hono<Env>()
 
 app.get('/', (c) => {
+  console.log('DATABASE_URL', c.env.DATABASE_URL)
   return c.text('Hello Hono!')
 })
 
