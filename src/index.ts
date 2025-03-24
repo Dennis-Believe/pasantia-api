91e18641f1a900a88332b3879a8d24cdbef8c567
Dennis Laurel <dennis.laurel.v@gmail.com>
2025-03-24 19:52:19 -0400

    probando env

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = new Hono<Env>()
 
 app.get('/', (c) => {
   console.log('DATABASE_URL', c.env.DATABASE_URL)
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!: ' + c.env.DATABASE_URL)
 })
 
 export default app
