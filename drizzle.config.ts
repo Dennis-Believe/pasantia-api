d0152609f7cb5f2ba12effc1d979cd8948783009
miguel zapata <miguel.zapata@believesol.com>
2025-03-25 18:02:33 -0400

    Actualización de dependencias en bun.lock y package.json, configuración de la base de datos en drizzle.config.ts, y eliminación de migraciones obsoletas. Se ajustaron importaciones en db.client.ts y se simplificó la estructura del esquema de usuarios.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit'
+import { env } from './src/config/env'
 
 export default defineConfig({
-  dialect: 'postgresql', 
+  dialect: 'postgresql',
   schema: './src/db/schema',
-  dbCredentials: {url:""},
-  out: "./src/db/migrations",
+  dbCredentials: {
+    url: env.databaseUrl,
+  },
 })
