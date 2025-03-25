2aa4169ad37f9a83ac6279dcdc4e90dc55cc3b79
William Quinones <willcas36@gmail.com>
2025-03-25 15:12:16 -0400

    primer commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono'
 // Make sure to install the 'postgres' package
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import * as schema from './db/schema'
-
-
 
 export type Env = {
   Bindings: {
@@ -15,12 +13,17 @@ export type Env = {
 const app = new Hono<Env>()
 
 app.get('/', async (c) => {
-  const queryClient = postgres(c.env.DATABASE_URL);
-  const db = drizzle({ client: queryClient, schema });
+  try {
+    const queryClient = postgres(c.env.DATABASE_URL)
+    const db = drizzle({ client: queryClient, schema })
 
-  const result = await db.query.users.findMany();
-  console.log('DATABASE_URL', result);
-  return c.text('Hello Hono!: ' + c.env.DATABASE_URL)
+    const result = await db.query.users.findMany()
+    console.log('DATABASE_URL', result)
+    return c.text('Hello Hono!: ' + c.env.DATABASE_URL)
+    
+  } catch (error) {
+    return c.json({ error: 'Hubo un error' })
+  }
 })
 
 export default app
