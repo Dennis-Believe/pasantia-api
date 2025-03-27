bbc9e3b1a0beacdffbbe7a7ce0b1f8168e0becf6
William Quinones <willcas36@gmail.com>
2025-03-27 15:11:45 -0400

    Se modifico el middleware de autenticacion de tipo session a token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,23 +1,25 @@
 import { Context, Next } from 'hono'
 import { verifyJwtToken } from '../modules/auth/utils/authUtils'
-import { getCookie } from 'hono/cookie'
+
 export const authenticate = async (c: Context, next: Next) => {
   try {
-    const token = getCookie(c,'token');
-    if (!token) {
+    const bearer = c.req.header('Authorization')
+    if (!bearer) {
       const error = new Error('No autorizado')
       return c.json(error)
     }
+    const token = bearer.split(' ')[1]
+
     try {
-        const decoded: any = await verifyJwtToken(token)
-        if(decoded && decoded.userId){
-            c.set('user',{id:decoded.userId})
-            await next()
-        }else{
-            return c.json({error:'Token no valido'})
-        }
+      const decoded: any = await verifyJwtToken(token)
+      if (decoded && decoded.userId) {
+        c.set('user', { id: decoded.userId })
+        await next()
+      } else {
+        return c.json({ error: 'Token no valido' })
+      }
     } catch (error) {
-        return c.json('Token no válido')
+      return c.json('Token no válido')
     }
   } catch (error) {
     return c.json('Hubo un error en la autenticación')
