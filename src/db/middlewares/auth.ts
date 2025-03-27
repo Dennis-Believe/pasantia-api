7838a85a84ed9a565b17061081d01ee86e8d36b2
William Quinones <willcas36@gmail.com>
2025-03-27 07:02:35 -0400

    Feat: Middleware de authenticacion mediante Session coockie

--- src/db/middlewares/auth.ts
+++ src/db/middlewares/auth.ts
@@ -1,29 +1,23 @@
 import { Context, Next } from 'hono'
-import { env } from '../../config/env'
 import { verifyJwtToken } from '../../modules/auth/utils/authUtils'
-import jwt from 'jsonwebtoken'
+import { getCookie } from 'hono/cookie'
 export const authenticate = async (c: Context, next: Next) => {
   try {
-    const bearer = c.req.header('Authorization')
-    if (!bearer) {
+    const token = getCookie(c,'token');
+    if (!token) {
       const error = new Error('No autorizado')
       return c.json(error)
     }
-    const token = bearer.split(' ')[1]
-    console.log(token)
-    if (!token) {
-      return c.json({ error: 'Token no válido' }, 401)
-    }
     try {
-      const decoded: any = jwt.verify(token, env.key)
-      if (decoded && decoded.userId) {
-       c.set('user', { id: decoded.userId })
-       await next()
-      } else {
-        return c.json({ error: 'Token no válido' })
-      }
+        const decoded: any = await verifyJwtToken(token)
+        if(decoded && decoded.userId){
+            c.set('user',{id:decoded.userId})
+            await next()
+        }else{
+            return c.json({error:'Token no valido'})
+        }
     } catch (error) {
-      return c.json({ error: 'Token no válido' }, 401)
+        return c.json('Token no válido')
     }
   } catch (error) {
     return c.json('Hubo un error en la autenticación')
