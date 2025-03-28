68b131b9078e4e4f5d8274798f91aec6f6e011ad
William Quinones <willcas36@gmail.com>
2025-03-27 22:42:17 -0400

    Feat: Creacion de sessions table y Migracion

---
+++ drizzle/0003_gigantic_namor.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "sessions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"userId" uuid NOT NULL,
+	"token" text NOT NULL,
+	"isEnabled" boolean DEFAULT true NOT NULL,
+	"lifeTime" integer NOT NULL,
+	"timeOut" time NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
